ee9dc5c546effcfafb037a6ac01b0def879dcfeb
Vivian Allen <mrvivianallen@gmail.com>
2018-04-26 14:15:16 +0100

    ItemHandler#get_adjustments, regular item passed sell-by test, failing

--- spec/item_handler_spec.rb
+++ spec/item_handler_spec.rb
@@ -6,10 +6,18 @@ describe ItemHandler do
   let(:handler) { ItemHandler.new() }
 
   describe "#get_adjustments" do
-    it '# returns {sellInAdj:-1, qualAdj: -1} for a regular object' do
-      item = [Item.new("foo", 0, 0)]
-      expected_hsh = {sellInAdj:-1, qualAdj: -1}
-      expect(handler.get_adjustments(item)).to eq(expected_hsh)
+
+    context "regular items" do
+      it '# returns {sellInAdj: -1, qualAdj: -1} for a regular object in sell-by date' do
+        item = [Item.new("foo", 10, 10)]
+        expected_hsh = {sellInAdj:-1, qualAdj: -1}
+        expect(handler.get_adjustments(item)).to eq(expected_hsh)
+      end
+      it '# returns {sellInAdj: -1, qualAdj: -2} for a regular object past sell-by date' do
+        item = [Item.new("foo", -1, 10)]
+        expected_hsh = {sellInAdj:-1, qualAdj: -2}
+        expect(handler.get_adjustments(item)).to eq(expected_hsh)
+      end
     end
   end
 
