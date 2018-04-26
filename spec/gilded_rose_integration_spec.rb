b445ae4365daf2a0fcc9fbc5d9bc88531f4111d2
Vivian Allen <mrvivianallen@gmail.com>
2018-04-26 18:26:19 +0100

    ItemAdjustmentHandler#get__adj for conjured items, added failing integration test

--- spec/gilded_rose_integration_spec.rb
+++ spec/gilded_rose_integration_spec.rb
@@ -7,5 +7,8 @@ describe GildedRose do
     it "matches the golden master (no conjured objects)" do
       expect(generate_GM(4, ITEMS_NO_CONJURED)).to eq(GM_4_DAYS_NO_CONJURED)
     end
+    it "matches the golden master (with conjured objects)" do
+      expect(generate_GM(4, ITEMS_W_CONJURED)).to eq(GM_4_DAYS_W_CONJURED)
+    end
   end
 end
--- spec/item_adjustment_handler_spec.rb
+++ spec/item_adjustment_handler_spec.rb
@@ -5,15 +5,15 @@ describe ItemAdjustmentHandler do
 
   let(:handler) { ItemAdjustmentHandler.new() }
 
-  describe "#get_adjustments" do
+  describe "#get_adj" do
 
     context "regular items" do
-      it '# returns {sellInAdj: -1, qualAdj: -1} for a regular object in sell-by date' do
+      it 'returns {sellInAdj: -1, qualAdj: -1} for a regular object in sell-by date' do
         item = Item.new("foo", 10, 10)
         expected_hsh = {sellInAdj:-1, qualAdj: -1}
         expect(handler.get_adj(item)).to eq(expected_hsh)
       end
-      it '# returns {sellInAdj: -1, qualAdj: -2} for a regular object past sell-by date' do
+      it 'returns {sellInAdj: -1, qualAdj: -2} for a regular object past sell-by date' do
         item = Item.new("foo", 0, 10)
         expected_hsh = {sellInAdj:-1, qualAdj: -2}
         expect(handler.get_adj(item)).to eq(expected_hsh)
@@ -21,12 +21,12 @@ describe ItemAdjustmentHandler do
     end
 
     context "aged brie" do
-      it '# returns {sellInAdj: -1, qualAdj: 1} for Aged Brie in sell-by date' do
+      it 'returns {sellInAdj: -1, qualAdj: 1} for Aged Brie in sell-by date' do
         item = Item.new("Aged Brie", 10, 10)
         expected_hsh = {sellInAdj:-1, qualAdj: 1}
         expect(handler.get_adj(item)).to eq(expected_hsh)
       end
-      it '# returns {sellInAdj: -1, qualAdj: 2} for Aged Brie past sell-by date' do
+      it 'returns {sellInAdj: -1, qualAdj: 2} for Aged Brie past sell-by date' do
         item = Item.new("Aged Brie", 0, 10)
         expected_hsh = {sellInAdj:-1, qualAdj: 2}
         expect(handler.get_adj(item)).to eq(expected_hsh)
@@ -34,7 +34,7 @@ describe ItemAdjustmentHandler do
     end
 
     context "legendary hand of Sulfuras or whatever, nerds" do
-      it '# returns {sellInAdj: 0, qualAdj: 0} for Sulfuras' do
+      it 'returns {sellInAdj: 0, qualAdj: 0} for Sulfuras' do
         item = Item.new("Sulfuras, Hand of Ragnaros", 0, 80)
         expected_hsh = {sellInAdj:0, qualAdj: 0}
         expect(handler.get_adj(item)).to eq(expected_hsh)
@@ -42,22 +42,28 @@ describe ItemAdjustmentHandler do
     end
 
     context "backstage passes" do
-      it '# returns {sellInAdj: -1, qualAdj: 1} for a backstage with more than 10 days to go' do
+      it 'returns {sellInAdj: -1, qualAdj: 1} for a backstage with more than 10 days to go' do
         item = Item.new("Backstage passes", 20, 10)
         expected_hsh = {sellInAdj: -1, qualAdj: 1}
         expect(handler.get_adj(item)).to eq(expected_hsh)
       end
-      it '# returns {sellInAdj: -1, qualAdj: 2} for a backstage with 10 days or less to go' do
+      it 'returns {sellInAdj: -1, qualAdj: 2} for a backstage with 10 days or less to go' do
         item = Item.new("Backstage passes", 10, 10)
         expected_hsh = {sellInAdj: -1, qualAdj: 2}
         expect(handler.get_adj(item)).to eq(expected_hsh)
       end
-      it '# returns {sellInAdj: -1, qualAdj: 3} for a backstage with 5 days or less to go' do
+      it 'returns {sellInAdj: -1, qualAdj: 3} for a backstage with 5 days or less to go' do
         item = Item.new("Backstage passes", 5, 10)
         expected_hsh = {sellInAdj: -1, qualAdj: 3}
         expect(handler.get_adj(item)).to eq(expected_hsh)
       end
     end
+    context "conjured items" do
+      it 'returns {sellInAdj: -1, qualAdj: -2} for a conjured item' do
+        item = Item.new("Conjured whatever", 10, 10)
+        expected_hsh = {sellInAdj: -1, qualAdj: 3}
+        expect(handler.get_adj(item)).to eq(expected_hsh)
+      end
+    end
   end
-
 end
